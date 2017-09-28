c458bce79d7fe6131d6f5a3f8c8b2f20c7fd6e65
Tobias Dussa <tobias.dussa@kit.edu>
2017-09-28 12:55:12 +0200

    Refactored entry code.

--- spectr-o-mat.py
+++ spectr-o-mat.py
@@ -33,9 +33,21 @@ def StringIsInt(s):
         return False
 
 
+
 class SpectrOMat:
     @staticmethod
-    def initialize(root, device='#0', scan_time=100000, reset_after=1, timestamp='%Y-%m-%dT%H:%M:%S%z', couple_times=True):
+    def initialize(root,
+                   autoexposure=False,
+                   autorepeat=False,
+                   autosave=True,
+                   dark_frames=1,
+                   device='#0',
+                   enable_plot=True,
+                   output_file='Snapshot-%Y-%m-%dT%H:%M:%S%z.dat',
+                   scan_frames=1,
+                   scan_time=100000,
+                   timestamp='%Y-%m-%dT%H:%M:%S%z',
+                   ):
         """Initialize the entire UI."""
         try:
             if (device[0] == '#'):
@@ -57,13 +69,15 @@ class SpectrOMat:
         SpectrOMat.button_startpause_texts = { True: 'Pause Measurement', False: 'Start Measurement' }
         SpectrOMat.button_stopdarkness_texts = { True: 'Stop Measurement', False: 'Get Darkness Correction' }
 
-        SpectrOMat.timestamp = timestamp
-        SpectrOMat.resetafter = StringVar()
-        SpectrOMat.scantime = StringVar()
-        SpectrOMat.darkframes = StringVar()
+        SpectrOMat.autoexposure = IntVar()
         SpectrOMat.autorepeat = IntVar()
         SpectrOMat.autosave = IntVar()
-        SpectrOMat.autoexposure = IntVar()
+        SpectrOMat.dark_frames = StringVar()
+        SpectrOMat.enable_plot = IntVar()
+        SpectrOMat.output_file = StringVar()
+        SpectrOMat.scan_frames = StringVar()
+        SpectrOMat.scan_time = StringVar()
+        SpectrOMat.timestamp = timestamp
         SpectrOMat.message = StringVar()
 
         # Initialize variables
@@ -75,25 +89,27 @@ class SpectrOMat:
         plot.ion()
 
         # Define the layout elements
-        SpectrOMat.reset_after_label = Label(root, text='Reset after\nn Measurements', justify=LEFT)
-        SpectrOMat.reset_after = Scale(root, from_=0, to=10000, showvalue=0, orient=HORIZONTAL, command=SpectrOMat.update_reset_after)
-        SpectrOMat.reset_after.set(reset_after)
-        SpectrOMat.reset_after_entry = Entry(root, textvariable=SpectrOMat.resetafter, validate='focusout', validatecommand=SpectrOMat.validate_reset_after)
+        SpectrOMat.label_scan_frames = Label(root, text='Scan Frame Count', justify=LEFT)
+        SpectrOMat.scale_scan_frames = Scale(root, from_=0, to=10000, showvalue=0, orient=HORIZONTAL, command=SpectrOMat.update_scan_frames)
+        SpectrOMat.scale_scan_frames.set(scan_frames)
+        SpectrOMat.entry_scan_frames = Entry(root, textvariable=SpectrOMat.scan_frames, validate='focusout', validatecommand=SpectrOMat.validate_scan_frames)
 
-        SpectrOMat.scan_time_label = Label(root, text='Scan Time [µs]', justify=LEFT)
-        SpectrOMat.scan_time = Scale(root, from_=SpectrOMat.spectrometer.minimum_integration_time_micros, to=10000000, showvalue=0, orient=HORIZONTAL, command=SpectrOMat.update_scan_time)
-        SpectrOMat.scan_time.set(scan_time)
-        SpectrOMat.scan_time_entry = Entry(root, textvariable=SpectrOMat.scantime, validate='focusout', validatecommand=SpectrOMat.validate_scan_time)
+        SpectrOMat.label_scan_time = Label(root, text='Scan Time [µs]', justify=LEFT)
+        SpectrOMat.scale_scan_time = Scale(root, from_=SpectrOMat.spectrometer.minimum_integration_time_micros, to=10000000, showvalue=0, orient=HORIZONTAL, command=SpectrOMat.update_scan_time)
+        SpectrOMat.scale_scan_time.set(scan_time)
+        SpectrOMat.entry_scan_time = Entry(root, textvariable=SpectrOMat.scan_time, validate='focusout', validatecommand=SpectrOMat.validate_scan_time)
 
-        SpectrOMat.dark_frames_label = Label(root, text='Dark Frame Count', justify=LEFT)
-        SpectrOMat.dark_frames = Scale(root, from_=1, to=10000, showvalue=0, orient=HORIZONTAL, command=SpectrOMat.update_dark_frames)
-        SpectrOMat.dark_frames.set(2) # This is necessary because the update function is not triggered if the value is set to the lower boundary (which is probably the default)
-        SpectrOMat.dark_frames.set(1)
-        SpectrOMat.dark_frames_entry = Entry(root, textvariable=SpectrOMat.darkframes, validate='focusout', validatecommand=SpectrOMat.validate_dark_frames)
+        SpectrOMat.label_dark_frames = Label(root, text='Dark Frame Count', justify=LEFT)
+        SpectrOMat.scale_dark_frames = Scale(root, from_=1, to=10000, showvalue=0, orient=HORIZONTAL, command=SpectrOMat.update_dark_frames)
+        SpectrOMat.scale_dark_frames.set(2) # This is necessary because the update function is not triggered if the value is set to the lower boundary (which is probably the default)
+        SpectrOMat.scale_dark_frames.set(1)
+        SpectrOMat.entry_dark_frames = Entry(root, textvariable=SpectrOMat.dark_frames, validate='focusout', validatecommand=SpectrOMat.validate_dark_frames)
 
         SpectrOMat.button_startpause_text = StringVar()
         SpectrOMat.button_startpause_text.set(SpectrOMat.button_startpause_texts[SpectrOMat.run_measurement])
         SpectrOMat.button_startpause = Button(root, textvariable=SpectrOMat.button_startpause_text, command=SpectrOMat.startpause)
+
+        SpectrOMat.checkbutton_enable_plot = Checkbutton(root, text='Enable Live Plotting', variable=SpectrOMat.enable_plot)
 
         SpectrOMat.checkbutton_autorepeat = Checkbutton(root, text='Auto Repeat', variable=SpectrOMat.autorepeat)
         SpectrOMat.checkbutton_autosave = Checkbutton(root, text='Auto Save', variable=SpectrOMat.autosave)
@@ -111,23 +127,24 @@ class SpectrOMat:
         SpectrOMat.message.set('Ready.')
 
         # Define the layout
-        SpectrOMat.reset_after_label.grid(rowspan=2)
-        SpectrOMat.reset_after.grid(row=0, column=1, rowspan=2)
-        SpectrOMat.reset_after_entry.grid(row=1, column=2)
+        SpectrOMat.label_scan_frames.grid(rowspan=2)
+        SpectrOMat.scale_scan_frames.grid(row=0, column=1, rowspan=2)
+        SpectrOMat.entry_scan_frames.grid(row=1, column=2)
 
-        SpectrOMat.scan_time_label.grid(rowspan=2)
-        SpectrOMat.scan_time.grid(row=2, column=1, rowspan=2)
-        SpectrOMat.scan_time_entry.grid(row=3, column=2)
+        SpectrOMat.label_scan_time.grid(rowspan=2)
+        SpectrOMat.scale_scan_time.grid(row=2, column=1, rowspan=2)
+        SpectrOMat.entry_scan_time.grid(row=3, column=2)
 
-        SpectrOMat.dark_frames_label.grid(rowspan=2)
-        SpectrOMat.dark_frames.grid(row=4, column=1, rowspan=2)
-        SpectrOMat.dark_frames_entry.grid(row=5, column=2)
+        SpectrOMat.label_dark_frames.grid(rowspan=2)
+        SpectrOMat.scale_dark_frames.grid(row=4, column=1, rowspan=2)
+        SpectrOMat.entry_dark_frames.grid(row=5, column=2)
 
         SpectrOMat.checkbutton_autorepeat.grid(row=6)
         SpectrOMat.checkbutton_autosave.grid(row=6, column=1)
         SpectrOMat.checkbutton_autoexposure.grid(row=6, column=2)
 
-        SpectrOMat.button_startpause.grid(row=7, columnspan=2)
+        SpectrOMat.button_startpause.grid(row=7)
+        SpectrOMat.checkbutton_enable_plot.grid(row=7, column=1)
         SpectrOMat.button_stopdarkness.grid(row=7, column=2)
 
         SpectrOMat.button_save.grid(row=8)
@@ -136,51 +153,58 @@ class SpectrOMat:
 
         SpectrOMat.textbox.grid(columnspan=3)
 
-    @staticmethod
-    def update_reset_after(newValue):
-        SpectrOMat.resetafter.set(newValue)
 
     @staticmethod
-    def validate_reset_after():
-        newValue = SpectrOMat.resetafter.get()
+    def update_scan_frames(newValue):
+        SpectrOMat.scan_frames.set(newValue)
+
+    @staticmethod
+    def validate_scan_frames():
+        newValue = SpectrOMat.scan_frames.get()
         if StringIsInt(newValue) and \
            int(newValue) >= 0 and \
            int(newValue) <= 10000:
-            SpectrOMat.reset_after.set(int(newValue))
+            SpectrOMat.scale_scan_frames.set(int(newValue))
         else:
-            SpectrOMat.resetafter.set(SpectrOMat.reset_after.get())
+            SpectrOMat.scan_frames.set(SpectrOMat.scale_scan_frames.get())
+            SpectrOMat.entry_scan_frames.after_idle(SpectrOMat.entry_scan_frames.config, {'validate': 'focusout', 'validatecommand': SpectrOMat.validate_scan_frames})
         return True
+
 
     @staticmethod
     def update_scan_time(newValue):
-        SpectrOMat.scantime.set(newValue)
+        SpectrOMat.scan_time.set(newValue)
         SpectrOMat.spectrometer.integration_time_micros(int(newValue))
 
     @staticmethod
     def validate_scan_time():
-        newValue = SpectrOMat.scantime.get()
+        newValue = SpectrOMat.scan_time.get()
         if StringIsInt(newValue) and \
            int(newValue) >= SpectrOMat.spectrometer.minimum_integration_time_micros and \
            int(newValue) <= 10000000:
-            SpectrOMat.scan_time.set(int(newValue))
+            SpectrOMat.scale_scan_time.set(int(newValue))
         else:
-            SpectrOMat.scantime.set(SpectrOMat.scan_time.get())
+            SpectrOMat.scan_time.set(SpectrOMat.scale_scan_time.get())
+            SpectrOMat.entry_scan_time.after_idle(SpectrOMat.entry_scan_time.config, {'validate': 'focusout', 'validatecommand': SpectrOMat.validate_scan_time})
         return True
+
 
     @staticmethod
     def update_dark_frames(newValue):
-        SpectrOMat.darkframes.set(newValue)
+        SpectrOMat.dark_frames.set(newValue)
 
     @staticmethod
     def validate_dark_frames():
-        newValue = SpectrOMat.darkframes.get()
+        newValue = SpectrOMat.dark_frames.get()
         if StringIsInt(newValue) and \
            int(newValue) >= 1 and \
            int(newValue) <= 10000:
-            SpectrOMat.dark_frames.set(int(newValue))
+            SpectrOMat.scale_dark_frames.set(int(newValue))
         else:
-            SpectrOMat.darkframes.set(SpectrOMat.dark_frames.get())
+            SpectrOMat.dark_frames.set(SpectrOMat.scale_scan_frames.get())
+            SpectrOMat.entry_dark_frames.after_idle(SpectrOMat.entry_dark_frames.config, {'validate': 'focusout', 'validatecommand': SpectrOMat.validate_dark_frames})
         return True
+
 
     @staticmethod
     def startpause():
@@ -259,9 +283,9 @@ class SpectrOMat:
     @staticmethod
     def measure():
         if SpectrOMat.run_measurement:
-            reset_after = SpectrOMat.reset_after.get()
-            if (reset_after > 0):
-                SpectrOMat.message.set('Scanning frame ' + str(SpectrOMat.measurement+1) + '/' + str(reset_after) + '...')
+            scan_frames = SpectrOMat.scan_frames.get()
+            if (scan_frames > 0):
+                SpectrOMat.message.set('Scanning frame ' + str(SpectrOMat.measurement+1) + '/' + str(scan_frames) + '...')
             else:
                 SpectrOMat.message.set('Scanning frame...')
             root.update()
@@ -289,8 +313,8 @@ class SpectrOMat:
             else:
                 print('.', end='', flush=True)
             plot.pause(0.0001)
-            if (reset_after > 0):
-                if SpectrOMat.measurement % reset_after == 0:
+            if (scan_frames > 0):
+                if SpectrOMat.measurement % scan_frames == 0:
                     #print(time.strftime(SpectrOMat.timestamp, time.gmtime()), SpectrOMat.data)
                     if SpectrOMat.autosave.get() != 0:
                         SpectrOMat.save()
@@ -304,10 +328,10 @@ class SpectrOMat:
         root.after(1, SpectrOMat.measure)
 
 
-def main(device='#0', scan_time=100000, reset_after=1, timestamp='%Y-%m-%dT%H:%M:%S%z'):
+def main(device='#0', scan_time=100000, scan_frames=1, timestamp='%Y-%m-%dT%H:%M:%S%z'):
     global root
     root.title('Spectr-O-Mat')
-    SpectrOMat.initialize(root, device, scan_time, reset_after, timestamp)
+    SpectrOMat.initialize(root, device, scan_time, scan_frames, timestamp)
     root.after(1, SpectrOMat.measure)
     root.mainloop()
 
@@ -316,9 +340,9 @@ if __name__ == "__main__":
     from argparse import ArgumentParser
     parser = ArgumentParser()
     parser.add_argument('-d', '--device', dest='device', default='#0', help='input device to use; "<serial number>" or "#<device number>" (default: #0)')
-    parser.add_argument('-r', '--reset_after', dest='reset_after', default='1', help='reset after n measurement cycles with 0 meaning indefinite (default: 1)')
+    parser.add_argument('-r', '--scan_frames', dest='scan_frames', default='1', help='reset after n measurement cycles with 0 meaning indefinite (default: 1)')
     parser.add_argument('-s', '--scan_time', dest='scan_time', default='100000', help='scan time in microseconds (default: 100000)')
     parser.add_argument('-t', '--timestamp',  dest='timestamp', default='%Y-%m-%dT%H:%M:%S%z', help='itemstamp format string (default: "%%Y-%%m-%%dT%%H:%%M:%%S%%z")')
     args = parser.parse_args()
 
-    main(args.device, args.scan_time, args.reset_after, args.timestamp)
+    main(args.device, args.scan_time, args.scan_frames, args.timestamp)
